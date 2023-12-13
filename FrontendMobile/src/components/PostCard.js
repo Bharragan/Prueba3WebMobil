353ee7732a3b1fb7b2c14d0e17c5709bec72dbcf
Bharragan <nicolas.henriquez01@alumnos.ucn.cl>
2023-12-13 15:54:26 -0300

    Feat: Finished app

--- FrontendMobile/src/components/PostCard.js
+++ FrontendMobile/src/components/PostCard.js
@@ -1,28 +1,84 @@
-// src/components/PostCard.js
-
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
-import { Card, Title, Paragraph } from 'react-native-paper';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 
-const PostCard = ({ post, comments }) => {
+const PostCard = ({ post, comments, onDelete }) => {
+  const styles = StyleSheet.create({
+    cardContainer: {
+      backgroundColor: '#fff',
+      borderRadius: 10,
+      marginVertical: 10,
+      padding: 15,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5,
+    },
+    postInfo: {
+      flex: 1,
+      marginLeft: 12,
+    },
+    postTitle: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginBottom: 8,
+    },
+    postAuthor: {
+      fontSize: 16,
+      color: '#555',
+    },
+    commentCount: {
+      fontSize: 14,
+      color: '#777',
+      marginTop: 4,
+    },
+    commentsContainer: {
+      marginTop: 8,
+    },
+    commentText: {
+      fontSize: 14,
+      color: '#333',
+    },
+    deleteButton: {
+      padding: 8,
+      borderRadius: 4,
+      backgroundColor: '#FF6262',
+    },
+    deleteButtonText: {
+      color: '#fff',
+      fontWeight: 'bold',
+    },
+    postImage: {
+      width: 80,
+      height: 80,
+      borderRadius: 40,
+    },
+  });
+
   return (
-    <Card>
-      <Card.Cover source={{ uri: post.image }} />
-      <Card.Content>
-        <Title>{post.title}</Title>
-        <Paragraph>Author: {post.author}</Paragraph>
-      </Card.Content>
-      <View>
-        <Text>Comments:</Text>
-        <FlatList
-          data={comments}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => <Text>{item.body}</Text>}
-        />
+    <View style={styles.cardContainer}>
+      <Image style={styles.postImage} source={{ uri: post.image }} />
+      <View style={styles.postInfo}>
+        <Text style={styles.postTitle}>{post.title}</Text>
+        <Text style={styles.postAuthor}>{`Autor: ${post.author}`}</Text>
+        <Text style={styles.commentCount}>{`Comentarios: ${comments.length}`}</Text>
+        <View style={styles.commentsContainer}>
+          {comments.map((comment) => (
+            <Text key={comment.id} style={styles.commentText}>{comment.body}</Text>
+          ))}
+        </View>
       </View>
-    </Card>
+      <TouchableOpacity style={styles.deleteButton} onPress={onDelete}>
+        <Text style={styles.deleteButtonText}>Eliminar</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
-//comentairo apra commit 
 
 export default PostCard;
--- FrontendMobile/src/screens/PostScreen.js
+++ FrontendMobile/src/screens/PostScreen.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { View, FlatList, Text } from 'react-native';
+import { View, FlatList, Text, TextInput, Alert } from 'react-native';
 import axios from 'axios';
 import PostCard from '../components/PostCard';
 
 // Define la dirección IP como constante
-const API_BASE_URL = 'http://192.168.193.183:3000';
+//const API_BASE_URL = 'http://192.168.193.183:3000';
+const API_BASE_URL = process.env.BACKEND_URL;
+console.log(API_BASE_URL);
 
 const PostScreen = () => {
   const [posts, setPosts] = useState([]);
   const [comments, setComments] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  const [searchAuthor, setSearchAuthor] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,6 +24,7 @@ const PostScreen = () => {
 
         setPosts(postsResponse.data);
         setComments(commentsResponse.data);
+        setFilteredPosts(postsResponse.data);
       } catch (error) {
         console.error('Error al obtener datos:', error);
       }
@@ -33,13 +38,62 @@ const PostScreen = () => {
     return comments.filter((comment) => comment.postId === postId);
   };
 
+  // Función para mostrar un modal de confirmación al intentar eliminar un post
+  const showDeleteConfirmation = (postId) => {
+    Alert.alert(
+      'Confirmar eliminación',
+      '¿Estás seguro de que deseas eliminar este post?',
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'Eliminar',
+          onPress: () => handleDeletePost(postId),
+          style: 'destructive',
+        },
+      ]
+    );
+  };
+
+  // Función para eliminar un post
+  const handleDeletePost = async (postId) => {
+    try {
+      await axios.delete(`${API_BASE_URL}/posts/${postId}`);
+      const updatedPosts = posts.filter((post) => post.id !== postId);
+      setPosts(updatedPosts);
+      setFilteredPosts(updatedPosts);
+    } catch (error) {
+      console.error('Error al eliminar post:', error);
+    }
+  };
+
+  // Función para filtrar posts por autor
+  const handleSearchByAuthor = () => {
+    const filtered = posts.filter(
+      (post) => post.author.toLowerCase().includes(searchAuthor.toLowerCase())
+    );
+    setFilteredPosts(filtered);
+  };
+
   return (
     <View>
+      <TextInput
+        placeholder="Buscar por autor"
+        value={searchAuthor}
+        onChangeText={(text) => setSearchAuthor(text)}
+        onSubmitEditing={handleSearchByAuthor}
+      />
       <FlatList
-        data={posts}
+        data={filteredPosts}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
-          <PostCard post={item} comments={getCommentsForPost(item.id)} />
+          <PostCard
+            post={item}
+            comments={getCommentsForPost(item.id)}
+            onDelete={() => showDeleteConfirmation(item.id)}
+          />
         )}
       />
     </View>
